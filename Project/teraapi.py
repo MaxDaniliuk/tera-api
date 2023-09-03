f3d33964e06d39fd57b98c6e2314d7d777bee2e9
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2023-09-03 18:46:31 +0300

    commit

--- Project/teraapi.py
+++ Project/teraapi.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+import dotenv
+import MySQLdb
+from fastapi import FastAPI, status, HTTPException
+from pydantic import BaseModel
+from typing import Any
+
+
+class CustomData(BaseModel):
+    standings: list[dict[str, Any]]
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Receive"}
+
+@app.post("/", status_code=201)
+async def process_posted_data(data: CustomData):
+    structred_data = data.standings
+    return {"message": "/data, hey post response"}
