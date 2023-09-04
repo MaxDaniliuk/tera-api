fdae2cf050f36226166bdc4b6f7e920a1f15109d
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2023-09-04 20:56:43 +0300

    commit

--- Api/teraapi.py
+++ Api/teraapi.py
@@ -1,9 +1,23 @@
-import dotenv
-import MySQLdb
 from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
 from typing import Any
+from dotenv import load_dotenv
+load_dotenv()
+import os
+#import MySQLdb
+import mysql.connector
 
+def create_db_connection():
+    connection = mysql.connector.connect(
+    host= os.getenv("DB_HOST"),
+    user=os.getenv("DB_USERNAME"),
+    passwd= os.getenv("DB_PASSWORD"),
+    db= os.getenv("DB_NAME"),
+    autocommit = True,
+    ssl_verify_identity = True,  # Enable SSL identity verification
+    ssl_ca = "/etc/ssl/cert.pem"
+    )
+    return connection
 
 class CustomData(BaseModel):
     standings: list[dict[str, Any]]
@@ -17,4 +31,4 @@ async def root():
 @app.post("/", status_code=201)
 async def process_posted_data(data: CustomData):
     structred_data = data.standings
-    return {"message": "/data, hey post response"}
+    return {"Post has been successful!"}
