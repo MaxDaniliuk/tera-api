12957be719bc15c6e311e6b4a8af2b580cca7b3a
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2023-09-05 23:01:56 +0300

    commit

--- Api/teraapi.py
+++ Api/teraapi.py
@@ -13,19 +13,17 @@ app = FastAPI()
 
 def create_db_connection():
     connection = mysql.connector.connect(
-    host= os.getenv("DB_HOST"),
-    user=os.getenv("DB_USERNAME"),
-    passwd= os.getenv("DB_PASSWORD"),
-    db= os.getenv("DB_NAME"),
-    autocommit = True,
-    ssl_verify_identity = True,  # Enable SSL identity verification
-    ssl_ca = "/etc/ssl/cert.pem",
-    charset='utf8mb4',  # Set the character set to utf8mb4
-    collation='utf8mb4_unicode_ci'
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USERNAME"),
+        passwd=os.getenv("DB_PASSWORD"),
+        db=os.getenv("DB_NAME"),
+        autocommit=True,
+        charset='utf8mb4',  # Explicitly set the character set
+        collation='utf8mb4_unicode_ci',  # Explicitly set the collation
+        ssl_verify_identity=True,
+        ssl_ca="/etc/ssl/cert.pem"
     )
     return connection
-
-
 
 
 def create_standings_table():
@@ -34,26 +32,24 @@ def create_standings_table():
         cursor = connection.cursor()
 
         table_name = "Standings_table"
-
+        column_definitions = [
+            "id INT AUTO_INCREMENT PRIMARY KEY",
+            "column1 VARCHAR(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",
+            "column2 VARCHAR(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",
+            "column3 VARCHAR(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",
+            "column4 VARCHAR(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",
+            "column5 VARCHAR(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",
+            "column6 VARCHAR(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",
+            "column7 VARCHAR(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",
+            "column8 VARCHAR(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",
+            "column9 VARCHAR(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",
+            "column10 VARCHAR(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",
+            "column11 VARCHAR(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+            ]
         
 
         # Check if the table exists, and create it if it doesn't
         if not table_exists(cursor, table_name):
-
-            column_definitions = [
-            "id INT AUTO_INCREMENT PRIMARY KEY",
-            "column1 VARCHAR(2)",
-            "column2 VARCHAR(25)",
-            "column3 VARCHAR(25)",
-            "column4 VARCHAR(2)",
-            "column5 VARCHAR(2)",
-            "column6 VARCHAR(2)",
-            "column7 VARCHAR(2)",
-            "column8 VARCHAR(3)",
-            "column9 VARCHAR(3)",
-            "column10 VARCHAR(4)",
-            "column11 VARCHAR(3)"
-            ]
 
             create_table_sql = f"CREATE TABLE {table_name} ({', '.join(column_definitions)})"
             cursor.execute(create_table_sql)
@@ -102,12 +98,17 @@ async def process_posted_data(data: CustomData):
         for dict_of_data in structred_data:
             
             insert_query = "INSERT INTO Standings_table (Vieta, Komanda, Logo, Rungtynės, Pergalės, Lygiosios, Pralaimėjimai, Įmušta, Praleista, Skirtumas, Taškai) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            row_values = tuple(dict_of_data.values())
+            row_values = tuple(value.encode("utf-8") if isinstance(value, str) else value for value in dict_of_data.values())
             cursor.execute(insert_query, row_values)
             row_count += 1
             print(f"row {row_count} has been added")
             
         connection.commit()
+
+        #################
+        #ERROR: {'detail': "1105 (HY000): syntax error at position 60 near 'Ä'"}
+
+        ################# # Will try to encode utf-8 for every string containing non-ascii char
 
         return {"message": "Data has been inserted successfully"}
     except Exception as e:
